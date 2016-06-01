ef0f06b3858452506377cab7f9190bd14a13932c
himynameisjeremy <himynameisjeremy@gmail.com>
2016-06-01 16:23:24 -0500

    Testing pop up now works

--- client/scripts/controllers/gardencontrols.js
+++ client/scripts/controllers/gardencontrols.js
@@ -5,10 +5,9 @@ sherrisGardenApp.controller('IndexController',  ['$scope', '$http', '$window','G
   $scope.toExtraView = function(){
     $window.location.href = '#extra';
   };
-
 }]);
 
-sherrisGardenApp.controller('GardenController',  ['$scope', '$http', '$window','GardenService',function($scope, $http, $window, GardenService) {
+sherrisGardenApp.controller('GardenController',  ['$scope', '$http', '$window','GardenService', '$mdDialog', function($scope, $http, $window, GardenService, $mdDialog) {
   $scope.gardenCreated = null;
   var checkIfThereIsData = function(){
         $http.get('/checkDB').then(function(response){
@@ -21,6 +20,25 @@ sherrisGardenApp.controller('GardenController',  ['$scope', '$http', '$window','
         });
   };
   checkIfThereIsData();
+  // createNewGarden = function(){
+  //   return console.log("Create New Garden pop up!");
+  // };
+  // $scope.createNewGarden = createNewGarden();
+
+  createNewGarden = function(id) {
+    var confirm = $mdDialog.confirm()
+          .title('This is a test dialog box.')
+          .ok('Yes')
+          .cancel('No');
+    $mdDialog.show(confirm).then(function() {
+      console.log("Yes");
+      // GardenService.plantAGarden();
+      // $scope.getGarden();
+    }, function() {
+      console.log("No");
+    });
+  };
+  $scope.createNewGarden = createNewGarden();
 
 }]);
 
--- client/scripts/factories/gardenservice.js
+++ client/scripts/factories/gardenservice.js
@@ -1,18 +1,8 @@
 sherrisGardenApp.factory("GardenService", ["$http", function($http){
 
-  // var gardenCreated = true;
 
-  // var checkIfThereIsData = function(){
-  //       $http.get('/checkDB').then(function(response){
-  //           var bool = response.data;
-  //           if(bool === false){
-  //            gardenCreated = false;
-  //           }
-  //       });
-  //   };
-  //   checkIfThereIsData();
 
     return {
-      // gardenCreated: gardenCreated
+
     };
 }]);
